982759b7fb332ad14109f57d2a204f5a90a8d1f3
wildhaber <wildhaber@godesign.ch>
2017-08-21 23:03:15 +0200

    add data.abstract.specs

--- src/data/data.abstract.spec.js
+++ src/data/data.abstract.spec.js
@@ -1,1 +1,49 @@
 import { DataAbstract } from './data.abstract';
+import { Subject } from 'rxjs/Subject';
+
+describe('DataAbstract', () => {
+
+    const dataPool = {};
+
+    const DA = new DataAbstract(
+        dataPool,
+    );
+
+    it('should exist', () => {
+        expect(typeof DataAbstract).toBe('function');
+    });
+
+    it('should have a constructor method defined', () => {
+        expect(DA.constructor).toBeDefined();
+        expect(typeof DA.constructor).toBe('function');
+    });
+
+    it('should have a getObservable method defined', () => {
+        expect(DA.getObservable).toBeDefined();
+        expect(typeof DA.getObservable).toBe('function');
+    });
+
+    it('should have a push method defined', () => {
+        expect(DA.push).toBeDefined();
+        expect(typeof DA.push).toBe('function');
+    });
+
+    it('should bind given dataPool to this._dataPool', () => {
+        expect(DA._dataPool).toEqual(dataPool);
+    });
+
+    it('should return observableSubject to this._observableSubject', () => {
+        expect(DA._observableSubject).toEqual(new Subject());
+    });
+
+    describe('#getObservable()', () => {
+
+        it('should return an RxJS Subject by default', () => {
+
+            it('should return observableSubject to this._observableSubject', () => {
+                expect(DA.getObservable()).toEqual(DA._observableSubject);
+            });
+        });
+    });
+
+});
