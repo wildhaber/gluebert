ceef3f04f95c9561eaf5ec4f3856997257356306
wildhaber <wildhaber@godesign.ch>
2017-08-22 20:59:08 +0200

    add spaces

--- template.js
+++ template.js
@@ -1,7 +1,7 @@
-import {HandlebarsTemplate} from './dist/template/handlebars.template';
-import {MustacheTemplate} from './dist/template/mustache.template';
-import {TemplateAbstract} from './dist/template/template.abstract';
-import {TwigTemplate} from './dist/template/twig.template';
+import { HandlebarsTemplate } from './dist/template/handlebars.template';
+import { MustacheTemplate } from './dist/template/mustache.template';
+import { TemplateAbstract } from './dist/template/template.abstract';
+import { TwigTemplate } from './dist/template/twig.template';
 
 export {
     HandlebarsTemplate,
